55e7a35716207a2bee1ff737884562eb22e1e767
Michelle Orwick <morwick8@gmail.com>
2020-02-12 15:28:42 -0700

    Add a Functioning Create Todo Field and Button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template # render template allows us to 
+from flask import Flask, render_template, request, redirect, url_for 
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) #this creates an app with the name being given the name of our file
@@ -14,6 +14,15 @@ def __repr__(self):
 	return f'<Todo {self.id} {self.description}>'
 
 db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+	description = request.form.get('description', '') #uses request from flask app to get post, enters empty string if nothing entered
+	# we use description variable to help us set up a new record in our database using the Todo model
+	todo = Todo(description=description)
+	db.session.add(todo)
+	db.session.commit() #this adds a record
+	return redirect(url_for('index')) #this will tell the view to reload index.html after updating the database
 
 @app.route('/') #sets up a route to listen to our homepage
 def index():
