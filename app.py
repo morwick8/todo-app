f317acd49a08af1396e06d772f180f62f25c31f5
Michelle Orwick <morwick8@gmail.com>
2020-02-17 10:36:21 -0700

    using sessions in controllers

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
-app = Flask(__name__) #this creates an app with the name being given the name of our file
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] ='postgres://michelle:Mobet-11@localhost:5432/todoapp'
-db = SQLAlchemy(app) #this db object links SQL alchemy to our flask app
+db = SQLAlchemy(app)
 
 class Todo(db.Model):
 	__tablename__ = 'todos'
@@ -13,18 +14,30 @@ class Todo(db.Model):
 def __repr__(self):
 	return f'<Todo {self.id} {self.description}>'
 
+db.create_all()
+
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-	description = request.get_json()['description'] #uses get_json instead
-	todo = Todo(description=description)
-	db.session.add(todo)
-	db.session.commit() #this adds a record
-	return jsonify({
-		'description': todo.description
-	}) #we are no longer reloading the html 
+	error = False
+	body = {}
+	try:
+		description = request.get_json()['description']
+		todo = Todo(description=description)
+		db.session.add(todo)
+		db.session.commit()
+		body['description'] = todo.description
+	except:
+		error = True
+		db.session.rollback()
+		print(sys.exc_info())
+	finally:
+		db.session.close()
+	if error:
+		abort (400)
+	else:
+		return jsonify(body)
 
-@app.route('/') #sets up a route to listen to our homepage
+@app.route('/')
 def index():
-	return render_template('index.html', data=Todo.query.all()
-		)
+    return render_template('index.html', data=Todo.query.all())
